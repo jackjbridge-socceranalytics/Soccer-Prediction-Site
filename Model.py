3090e1eedb204e86217e11a76e08fda010269654
Jack Bridge <73715318+jackjbridge-socceranalytics@users.noreply.github.com>
2020-12-29 22:12:50 -0600

    Adding code used to build model
    Built model, and am maintaining/updating in a JupyterHub notebook. Posting code here for potential feedback

---
+++ Model.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+
+shoot_stats = pd.read_csv('/Users/richardbridge/Downloads/Shooting Stats.csv')
+shoot_stats
+
+Team_ID_Map = {1: 'Arsenal', 2: 'Aston Villa', 3: 'Brighton', 4:'Burnley', 5: 'Chelsea', 6: 'Crystal Palace', 7:'Everton', 8: 'Fulham', 9: 'Leeds United', 10: 'Leicester City',
+              11: 'Liverpool', 12: 'Manchester City', 13: 'Manchester Utd', 14: 'Newcastle Utd', 15: 'Sheffield Utd', 16: 'Southampton', 17: 'Tottenham', 18: 'West Brom',
+              19: 'West Ham', 20: 'Wolves', 21: 'Bournemouth', 22: 'Norwich City', 23: 'Watford', 24: 'Cardiff City', 25: 'Huddersfield', 26: 'Stoke City', 27: 'Swansea City',
+              29: 'Blackburn Rovers FC ', 30: 'Queens Park Rangers FC ', 31: 'Sheffield Wednesday FC '}
+for i, row in shoot_stats.iterrows():
+
+    Result = shoot_stats.at[i, 'Result']
+    Team = shoot_stats.at[i, 'Team']
+    Opponent = shoot_stats.at[i, 'Opponent']
+    if Result == 'W':
+        shoot_stats.at[i, 'winningTeam'] = list(Team_ID_Map.keys())[list(Team_ID_Map.values()).index(Team)]
+        shoot_stats.at[i, 'losingTeam'] = list(Team_ID_Map.keys())[list(Team_ID_Map.values()).index(Opponent)]
+    elif Result == 'D':
+        shoot_stats.at[i, 'winningTeam'] = 0
+        shoot_stats.at[i, 'losingTeam'] = 0
+    else:
+        shoot_stats.at[i, 'winningTeam'] = list(Team_ID_Map.keys())[list(Team_ID_Map.values()).index(Opponent)]
+        shoot_stats.at[i, 'losingTeam'] = list(Team_ID_Map.keys())[list(Team_ID_Map.values()).index(Team)]
+shoot_stats.head()
+
+for i, row in shoot_stats.iterrows():
+    Team = shoot_stats.at[i, 'Team']
+    Opponent = shoot_stats.at[i, 'Opponent']
+    shoot_stats.at[i, 'Team'] = list(Team_ID_Map.keys())[list(Team_ID_Map.values()).index(Team)]
+    shoot_stats.at[i, 'Opponent'] = list(Team_ID_Map.keys())[list(Team_ID_Map.values()).index(Opponent)]
+shoot_stats.head()
+
+shoot_stats['winningTeam'] = shoot_stats['winningTeam'].astype(int)
+#shoot_stats['GF'] = shoot_stats['GF'].astype(int)
+
+train_X = np.asarray(average_goals[['Team','Opponent', 'GA','GF']])
+train_y = np.asarray(average_goals.winningTeam)
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(criterion = 'entropy', random_state = 42)
+rfc.fit(train_X, train_y)
+
+rfc_pred_train = rfc.predict(train_X)
+print('Training Set Evaluation F1-Score=>',f1_score(train_y,rfc_pred_train,average='micro'))
+
+test_set = pd.read_csv('/Users/richardbridge/Downloads/Premier2021.csv')
+Team_ID_Map = {1: 'Arsenal', 2: 'Aston Villa', 3: 'Brighton', 4:'Burnley', 5: 'Chelsea', 6: 'Crystal Palace', 7:'Everton', 8: 'Fulham', 9: 'Leeds', 10: 'Leicester',
+              11: 'Liverpool', 12: 'Man City', 13: 'Man Utd', 14: 'Newcastle', 15: 'Sheffield United', 16: 'Southampton', 17: 'Tottenham', 18: 'West Brom',
+              19: 'West Ham', 20: 'Wolves', 21: 'AFC Bournemouth ', 22: 'Norwich City', 23: 'Watford FC ', 24: 'Cardiff City', 25: 'Huddersfield Town AFC ', 26: 'Stoke City FC ', 27: 'Swansea City AFC ',
+              29: 'Blackburn Rovers FC ', 30: 'Queens Park Rangers FC ', 31: 'Sheffield'}
+test_set1 = test_set[['Home','Away', 'Result']]
+test_set1["Team 1"] = 0
+test_set1["Team 2"] = 0
+test_set1["GGD"] = 0
+test_set1["winningTeam"] = "None"
+for i, row in test_set1.iterrows():
+    result = test_set1.at[i, 'Result']
+    team1Score = result.split("-", 1)[0]
+    team2Score = result.split("-", 1)[1]
+
+    Team1 = str(test_set1.at[i, 'Home'])
+    Team2 = str(test_set1.at[i, 'Away'])
+
+    test_set1.at[i, "Team"] = list(Team_ID_Map.keys())[list(Team_ID_Map.values()).index(Team1)]
+    test_set1.at[i,"Opponent"] = list(Team_ID_Map.keys())[list(Team_ID_Map.values()).index(Team2)]
+
+    test_set1.at[i, 'GGD'] = abs(int(team2Score) - int(team1Score))
+    test_set1.at[i, 'GA'] = int(team2Score)
+    test_set1.at[i, 'GF'] = int(team1Score)
+    if team1Score == team2Score:
+        test_set1.at[i, 'winningTeam'] = 0
+    elif team1Score > team2Score:
+        test_set1.at[i, 'winningTeam'] = list(Team_ID_Map.keys())[list(Team_ID_Map.values()).index(Team1)]
+    else:
+        test_set1.at[i, 'winningTeam'] = list(Team_ID_Map.keys())[list(Team_ID_Map.values()).index(Team2)]
+test_set1['Team'] = test_set1['Team'].astype(int)
+test_set1['Opponent'] = test_set1['Opponent'].astype(int)
+test_set1['GA'] = test_set1['GA'].astype(int)
+test_set1['GF'] = test_set1['GF'].astype(int)
+test_set1.head()
+
+test_set1['winningTeam'] = test_set1['winningTeam'].astype(int)
+test_X = np.asarray(test_set1[['Team','Opponent', 'GA', 'GF']])
+test_y = np.asarray(test_set1.winningTeam)
+
+rfc_pred_test = rfc.predict(test_X)
+print('Testing Set Evaluation F1-Score=>',f1_score(test_y,rfc_pred_test,average='micro'))
